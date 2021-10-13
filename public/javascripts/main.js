9a155708fca1458714bbb7baffc6f9c5da9f3269
Hareem Akram <hakram@hawk.iit.edu>
2021-10-13 03:39:11 -0500

    created individual functions for signaling channel events

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -28,3 +28,28 @@ const sc = io( `/${namespace}`, { autoConnect: false });
 button.addEventListener('click', function() {
     sc.open();
 });
+
+/* Signaling channel events */
+
+function registerScEvents() {
+    sc.on('connect', handleScConnect);
+    sc.on('connected peer', handleScConnectedPeer);
+    sc.on('signal', handleScSignal);
+    sc.on('disconnected peer', handleScDisconnectedPeer);
+}
+
+function handleScConnect() {
+    console.log('Connected to signaling server.');
+}
+
+function handleScConnectedPeer() {
+    console.log('Heard a peer connect.');
+}
+
+async function handleScSignal() {
+    console.log('Heard a signal.');
+}
+
+function handleScDisconnectedPeer() {
+    console.log('Heard a peer disconnect.');
+}
